2d1b0be3e67ad180cf581be478630ebd786ed83f
sree <rajan.sreeraj@yahoo.com>
2021-05-03 22:26:06 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import schedule
+import time
+from vaccination_slots import find_slots
+
+# def job():
+#     find_slots();
+
+# schedule.every(1).minutes.do(job)
+
+# while 1:
+#     schedule.run_pending()
+#     time.sleep(1)
+
+find_slots()
---
+++ vaccination_slots.py
@@ -0,0 +1,68 @@
+import json
+import requests
+import os
+from datetime import datetime, timedelta
+
+API_ENDPOINT_URL = "https://cdn-api.co-vin.in/api"
+
+slot_endpoint = "/v2/appointment/sessions/public/calendarByPin"
+pincodes = ['400074', '410210', '400614', '400071', '400706']
+week_range = 2
+available_capacity = 0 # testing since zero vaccines thx modi
+slack_api_endpoint = ""
+
+def find_slots():
+    filtered_centers = {}
+    today = datetime.today()
+
+    for i in range(week_range):
+        day = today + timedelta(days=(i * 7))
+        day = day.strftime('%d-%m-%Y')
+
+        for pincode in pincodes:
+            params = {'pincode':pincode, 'date': day}
+            centers = call_vaccination_slot_api(params=params)
+            filter_centers(centers=centers, filtered_centers=filtered_centers)
+
+    send_notification(filtered_centers=filtered_centers)
+
+
+def filter_centers(centers, filtered_centers):
+  for center in centers:
+    for session in center['sessions']:
+      if session['available_capacity'] >= available_capacity:
+        process_center(filtered_centers, center, session)
+  return filtered_centers
+
+
+def process_center(filtered_centers, center, session):
+  if center['center_id'] in filtered_centers:
+    filtered_centers[center['center_id']]['sessions'].append(session)
+  else:
+    filtered_centers[center['center_id']] = center
+    filtered_centers[center['center_id']]['sessions'] = [session]
+
+
+def send_notification(filtered_centers):
+    message = "Hey following vaccinations centres are available:  \n"
+    for i, key in enumerate(filtered_centers.keys()):
+        center = filtered_centers[key]
+        message += "\n{} *{}* {}\n".format(i+1, center["name"], center["pincode"])
+        for session in filtered_centers[key]["sessions"]:
+            message += "\tDate {} Available capacity {} Vaccine {}\n".format(session["date"], session["available_capacity"], session["vaccine"])
+    send_slack_message(message=message)
+
+
+def send_slack_message(message):
+    s = requests.Session()
+    s.headers.update({'Content-type': 'application/json'})
+    s.post(url=slack_api_endpoint, json={"text": message})
+
+
+def call_vaccination_slot_api(params):
+  s = requests.Session()
+  s.headers.update({'Accept-Language': 'en-US'})
+  url = API_ENDPOINT_URL + slot_endpoint
+  response = s.get(url=url, params=params)
+  data = response.json()
+  return data['centers']
