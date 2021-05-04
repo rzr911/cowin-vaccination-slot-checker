2f5dfbf9ff66f3e1127e5835508fa45afcd16c93
sree <rajan.sreeraj@yahoo.com>
2021-05-04 19:22:14 +0530

    add cache rules

--- vaccination_slots.py
+++ vaccination_slots.py
@@ -11,7 +11,7 @@ import redis
 
 env = Env()
 env.read_env()
-r = redis.Redis()
+r = redis.Redis(decode_responses=True)
 
 API_ENDPOINT_URL = "https://cdn-api.co-vin.in/api"
 
@@ -19,7 +19,7 @@ slot_endpoint = "/v2/appointment/sessions/public/calendarByPin"
 week_range = env.int("WEEK_RANGE")
 available_capacity = env.int("AVAILABILITY", 1)
 slack_token = env('slack_token')
-
+cache_expiration = env.int("CACHE_EXPIRATION", 6)
 
 users = []
 
@@ -33,7 +33,7 @@ def load_users(users):
             pincodes = row[1].split("-")
             users.append({"name": row[0], "pincodes": pincodes, "user_id": row[2]})
         line_count +=1
-        
+
 
 
 def find_slots():
@@ -43,18 +43,17 @@ def find_slots():
     today = datetime.today()
 
     for user in users:
-      if not r.get(user["user_id"]):
-        for i in range(week_range):
-            day = today + timedelta(days=(i * 7))
-            day = day.strftime('%d-%m-%Y')
+      for i in range(week_range):
+          day = today + timedelta(days=(i * 7))
+          day = day.strftime('%d-%m-%Y')
 
-            for pincode in user["pincodes"]:
-                params = {'pincode': pincode, 'date': day}
-                centers = call_vaccination_slot_api(params=params)
-                filter_centers(centers=centers, filtered_centers=filtered_centers)
-        if filtered_centers:
-          send_notification(filtered_centers=filtered_centers, user_id=user["user_id"])
-        filtered_centers = {}
+          for pincode in user["pincodes"]:
+              params = {'pincode': pincode, 'date': day}
+              centers = call_vaccination_slot_api(params=params)
+              filter_centers(centers=centers, filtered_centers=filtered_centers)
+      if filtered_centers:
+        send_notification(filtered_centers=filtered_centers, user=user)
+      filtered_centers = {}
 
 
 def filter_centers(centers, filtered_centers):
@@ -73,16 +72,22 @@ def process_center(filtered_centers, center, session):
     filtered_centers[center['center_id']]['sessions'] = [session]
 
 
-def send_notification(filtered_centers, user_id):
-    r.setex(user_id, timedelta(hours=2), "set")
-    # r.setex(user_id, timedelta(minutes=2), "set")
-    message = "Hey following vaccinations centres are available:  \n"
+def send_notification(filtered_centers, user):
+    message = "Hey {}, following vaccination centres are available:  \n".format(user["name"])
+
     for i, key in enumerate(filtered_centers.keys()):
         center = filtered_centers[key]
         message += "\n{} *{}* {}\n".format(i+1, center["name"], center["pincode"])
         for session in filtered_centers[key]["sessions"]:
-            message += "\tDate {} Available capacity {} Vaccine {}\n".format(session["date"], session["available_capacity"], session["vaccine"])
-    send_slack_message(message=message, user_id=user_id)
+            message += "\tDate *{}* Available capacity *{}* Vaccine *{}*\n".format(session["date"], session["available_capacity"], session["vaccine"])
+
+    cached_result_length = 0
+    if r.get(user["user_id"]):
+      cached_result_length = int(r.get(user["user_id"]))
+
+    if not r.get(user["user_id"]) or int(cached_result_length) != len(filtered_centers.keys()):
+      r.setex(user["user_id"], timedelta(hours=cache_expiration), len(filtered_centers.keys()))
+      send_slack_message(message=message, user_id=user["user_id"])
 
 
 def send_slack_message(message, user_id):
@@ -91,10 +96,11 @@ def send_slack_message(message, user_id):
     api_method='chat.postMessage',
     json={'channel': user_id,'text': message}
   )
-  client.api_call(
-    api_method='chat.postMessage',
-    json={'channel': "UGNCYFTBP",'text': message}
-  )
+  if user_id != "UGNCYFTBP":
+    client.api_call(
+      api_method='chat.postMessage',
+      json={'channel': "UGNCYFTBP",'text': message}
+    )
 
 
 def call_vaccination_slot_api(params):
