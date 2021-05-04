36a7e13e44599d580742c57aaf60a1dac4deb584
sree <rajan.sreeraj@yahoo.com>
2021-05-04 12:16:40 +0530

    add pyenv path

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 cd cowin-vaccination-slot-checker/
-echo $HOME;
+echo $HOME;export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 pyenv activate cowin
 pip install -r requirements.txt
 pkill -9 -f main_cowin.py
