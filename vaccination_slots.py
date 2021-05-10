9ba3f2ec3abf0775d969bea902e271e6e2fe8b85
sree <rajan.sreeraj@yahoo.com>
2021-05-10 12:00:40 +0530

    add user

--- vaccination_slots.py
+++ vaccination_slots.py
@@ -27,9 +27,9 @@ def load_users(users):
   with open('users.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
+    
     for row in csv_reader:
         if line_count != 0:
-            # print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
             pincodes = row[1].split("-")
             users.append({"name": row[0], "pincodes": pincodes, "user_id": row[2]})
         line_count +=1
